6989ac5895ecbe4ac7bca8ae22470a8b0108ab16
Felipe Moyano <gafemoyano@hotmail.com>
2019-08-16 12:54:10 -0500

    use graphql

--- App.tsx
+++ App.tsx
@@ -2,17 +2,18 @@ import React from "react"
 import { ApolloClient } from "apollo-client"
 import { createHttpLink } from "apollo-link-http"
 import { InMemoryCache } from "apollo-cache-inmemory"
+import gql from "graphql-tag"
 import VenueScreen from "./src/components/VenueScreen"
 
 import { ApolloProvider } from "@apollo/react-hooks"
 
 export const link = createHttpLink({
-  uri: "/graphql"
+  uri: "http://192.168.0.7:3000/graphql/"
 })
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
+  link: link
 })
 
 export default function App() {
--- src/components/VenueScreen.tsx
+++ src/components/VenueScreen.tsx
@@ -1,14 +1,15 @@
 import React from "react"
 import gql from "graphql-tag"
 import { StyleSheet, Text, View } from "react-native"
+import { useQuery } from "@apollo/react-hooks"
 
 const VENUE_QUERY = gql`
   query VenueQuery {
     venue(id: "fit-for-all-sede-calle-94") {
+      id
       name
       city
       contactEmail
-      id
       cover
       activityCount
       address
@@ -26,9 +27,17 @@ const styles = StyleSheet.create({
 })
 
 function VenueScreen() {
+  const { loading, error, data } = useQuery(VENUE_QUERY)
+  if (loading) return <Text>Loading...</Text>
+  if (error) return <Text>Error! ${error.message}</Text>
+  let venue = data.venue
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{venue.name}</Text>
+      <Text>{venue.city}</Text>
+      <Text>{venue.contactEmail}</Text>
+      <Text>{venue.address}</Text>
+      <Text>{venue.cover}</Text>
     </View>
   )
 }
