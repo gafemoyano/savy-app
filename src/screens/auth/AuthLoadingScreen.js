42be58925e44252d1fdc30cf695540fe8e0b9e3c
lstaleroc <luchotalcha@gmail.com>
2019-09-17 15:09:14 -0500

    Fix auth flow

--- src/screens/auth/AuthLoadingScreen.js
+++ src/screens/auth/AuthLoadingScreen.js
@@ -13,7 +13,7 @@ class AuthLoadingScreen extends React.Component {
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(userToken ? "Explore" : "Explore")
+    this.props.navigation.navigate(userToken ? "Explore" : "Auth")
   }
 
   // Render any loading content that you like here
