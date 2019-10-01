c0f601501a6154a21dfa9fc85a4f02e008a5d8d9
lstaleroc <luchotalcha@gmail.com>
2019-10-01 11:17:43 -0500

    update app.js

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({ uri: "http://192.168.0.7:3000/graphql" })
+  link: new HttpLink({ uri: "http://192.168.0.14:3000/graphql" })
 })
 
 const styles = StyleSheet.create({
